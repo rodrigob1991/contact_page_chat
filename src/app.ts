d02b3a9c03ddcaee5b26892c596e37676e7253ce
rodrigob1991 <rodrigito1991@hotmail.com>
2022-12-17 18:30:19 -0300

    several changes

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,27 @@ import http from "http"
 import dotenv from "dotenv"
 import { v4 as uuidv4 } from 'uuid'
 
-type OutboundMessageType = "con" | "dis" | "mes" | "ak"
-type InboundMessageType = "mes" | "ak"
-type OutboundHostMessage<T extends OutboundMessageType> = `${T}:${string}${T extends ("con" | "dis") ? "" : `:${number}:${string}`}`
-type InboundHostMessage<T extends InboundMessageType> = `${T}:${string}:${number}`
-type OutboundGuessMessage<T extends OutboundMessageType> = `${T}:${number}${T extends "mes" ? `:${string}` : ""}`
-type InboundGuessMessage<T extends InboundMessageType> = `${T}:${number}${T extends "mes" ? `:${string}` : ""}`
-type OutboundMessage<T extends OutboundMessageType> = OutboundHostMessage<T> | OutboundGuessMessage<T>
-type SendMessage<O extends OutboundMessage<OutboundMessageType>> = <T extends OutboundMessageType>(t: T, m: string) => void
-type SubscribeToMessages = (sendMessage: SendMessage, isHost: boolean,  guessId?: string) => void
-type PublishMessage = <H extends boolean, MT extends MessageType>(messageType: MT, m: MT extends "mes" ? string : "", isHost: H,  guessId: H extends true ? undefined : string) => void
+type UserType = "host" | "guess"
+
+type OutboundToHostMesMessage = `mes:${string}:${number}:${string}`
+type OutboundToHostConMessage = `con:${string}:${number}`
+type OutboundToHostDisMessage = `dis:${string}:${number}`
+type OutboundToHostAckMessage = `ack:${number}`
+type OutboundToGuessMesMessage = `mes:${number}:${string}`
+type OutboundToGuessConMessage = `con:${number}`
+type OutboundToGuessDisMessage = `dis:${number}`
+type OutboundToGuessAckMessage = `ack:${number}`
+
+type InboundFromHostMesMessage = `mes:${string}:${number}:${string}`
+type InboundFromHostAckMessage = `ack:${number}`
+type InboundFromGuessMesMessage = `mes:${number}:${string}`
+type InboundFromGuessAckMessage = `ack:${number}`
+
+//type HostGuess<T extends string> = T extends string ? `${T}-host` |  `${T}-guess` : never
+type MessagePrefix = "con" | "dis" | "mes" | "ack"
+type SendMessage = (mp: MessagePrefix, payload?: string) => void
+type SubscribeToMessages = (sendMessage: SendMessage, userType: UserType, guessId?: string) => void
+type PublishMessage = (mp: MessagePrefix, payload: string, userType: UserType, guessId?: string) => void
 
 dotenv.config()
 
@@ -56,7 +67,7 @@ const initWebSocket = (subscribeToMessages : SubscribeToMessages, publishMessage
             const connection = request.accept("echo-protocol", origin)
             console.log((new Date()) + " connection accepted")
 
-            const sendMessage: SendMessage = (m) => { connection.sendUTF(m) }
+            const sendMessage: SendMessage = (mp, payload) => { connection.sendUTF() }
 
             const isHostUser = request.httpRequest.headers.host_user === process.env.HOST_USER_SECRET
             const guessId = isHostUser ? undefined : uuidv4()
@@ -79,32 +90,47 @@ const initWebSocket = (subscribeToMessages : SubscribeToMessages, publishMessage
 const init = async () => {
     const redisClient = await initRedisConnection()
 
-    const connectionsChannelPrefix = "connections"
-    const disconnectionsChannelPrefix = "disconnections"
-    const messagesChannelPrefix = "messages"
     const suffixHost = "host"
     const suffixGuess = "guess"
-    const getConnectionsChannel = (isHost: boolean) =>  connectionsChannelPrefix + "-" + (isHost ? suffixHost : suffixGuess)
-    const getDisconnectionsChannel = (isHost: boolean) =>  disconnectionsChannelPrefix + "-" + (isHost ? suffixHost : suffixGuess)
-    const getMessagesChannel = (isHost: boolean, guessId?: string) => messagesChannelPrefix + "-" + (isHost ? suffixHost : suffixGuess + "-" + guessId)
+    const getConnectionsChannel = (userType: UserType) =>  "con" + "-" + (userType === "host" ? suffixHost : suffixGuess)
+    const getDisconnectionsChannel = (userType: UserType) =>  "dis" + "-" + (userType === "host" ? suffixHost : suffixGuess)
+    const getMessagesChannel = (userType: UserType, guessId?: string) => "mes" + "-" + (userType === "host" ? suffixHost : suffixGuess + "-" + guessId)
+    const getAcknowledgmentChannel = (userType: UserType, guessId?: string) => "ack" + "-" + (userType === "host" ? suffixHost : suffixGuess + "-" + guessId)
 
-    const subscribeToMessages: SubscribeToMessages = async (sendMessage, isHost, guessId) => {
+    const subscribeToMessages: SubscribeToMessages = async (sendMessage, userType, guessId) => {
         const subscriber = redisClient.duplicate()
         await subscriber.connect()
 
-        await subscriber.subscribe(getConnectionsChannel(isHost), (message, channel) => {
-            sendMessage("con")
+        await subscriber.subscribe(getConnectionsChannel(userType), (message, channel) => {
+            sendMessage("con", userType === "host" ? message : undefined)
         })
-        await subscriber.subscribe(getDisconnectionsChannel(isHost), (message, channel) => {
-            sendMessage("dis")
+        await subscriber.subscribe(getDisconnectionsChannel(userType), (message, channel) => {
+            sendMessage("dis", userType === "host" ? message : undefined)
         })
-        await subscriber.subscribe(getMessagesChannel(isHost, guessId), (message, channel) => {
-            sendMessage(("mes-" + message) as `mes-${string}`)
+        await subscriber.subscribe(getMessagesChannel(userType, guessId), (message, channel) => {
+            sendMessage("mes", message)
+        })
+        await subscriber.subscribe(getAcknowledgmentChannel(userType, guessId), (message, channel) => {
+            sendMessage("ack", message)
         })
     }
-    const publishMessage: PublishMessage = (type, m, isHost, guessId) => {
-        const channel = type === "con" ? getConnectionsChannel(isHost) : type === "dis" ? getDisconnectionsChannel(isHost) : getMessagesChannel(isHost, guessId)
-        redisClient.publish(channel, isHost ? m : "from{" + guessId + "}" + m)
+    const publishMessage: PublishMessage = (mp, payload, userType, guessId) => {
+        let channel
+        switch (mp) {
+            case "con":
+                channel = getConnectionsChannel(userType)
+                break
+            case "dis":
+                channel = getDisconnectionsChannel(userType)
+                break
+            case "mes":
+                channel = getMessagesChannel(userType, guessId)
+                break
+            case "ack":
+                channel = getAcknowledgmentChannel(userType, guessId)
+                break
+        }
+        redisClient.publish(channel, payload)
     }
 
     initWebSocket(subscribeToMessages, publishMessage)
